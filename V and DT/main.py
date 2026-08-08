15b6c2dfda02b8a24febbdde0d63890729c6d10e
mochaa21 <mochamadsyahid573@gmail.com>
2026-08-08 21:44:55 +0700

    feat: add function confirm and logic

--- V and DT/main.py
+++ V and DT/main.py
@@ -1,5 +1,18 @@
 # Abyy
 
+# function
+
+def confirm_action():
+    while True:
+        response = input("Do you want to continue? (y/n) -> ").strip().lower()
+
+        # logic
+        if response in ['y', 'yes']:
+            return True
+        if response in ['n', 'no']:
+            return False
+        print("Invalid input. Please enter 'y' or 'n'.")
+
 # need insert: v, i, f, b
 
 # v
@@ -11,6 +24,31 @@ age = int(input("Your age: "))
 
 
 # result of v and i
-print()
+print(f"Your submission has been successfully recorded...\nName: {name}\nEmail: {email}\nAge: {age}")
 
 # f
+price = 10.99
+
+if confirm_action():
+    budget = int(input("Input your budget: "))
+
+    def confirm_action_store():
+        while True:
+            response = input(f"Do you want to buy? the price is ${price} (y/n) -> ").strip().lower()
+
+            # logic
+            if response in ['y', 'yes']:
+                return True
+            if response in ['n', 'no']:
+                return False
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+    if confirm_action_store():
+        budget = budget - price
+        print(f"Success! Your budget now is ${budget}")
+    else:
+        print('Action Cancelled.')
+
+else:
+    print('Action Cancelled.')
+
