b21ddb6799e7170081f7882faa22c332d784fc93
mochaa21 <mochamadsyahid573@gmail.com>
2026-08-09 11:59:12 +0700

    update: adding comment in task 1 dile

--- task 1/main.py
+++ task 1/main.py
@@ -1,3 +1,14 @@
+"""
+Bisakah lu bikin sebuah fungsi bernama cek_sinyal(market) yang mengekstrak "Harga Saat Ini", "Stop Loss", dan "Take Profit" dari data di atas?
+Lalu, buat aturan if/else:
+
+Kalau Harga Saat Ini >= Take Profit, cetak "Take Profit Tersentuh! Jual sekarang."
+
+Kalau Harga Saat Ini <= Stop Loss, cetak "Stop Loss Tersentuh! Cut loss sekarang."
+
+Selain itu, cetak "Harga masih aman, tahan posisi."
+"""
+
 market_data = {
     'ticker': 'XAUUSD',
     'prices': [2045.50, 2055.00], # [Harga Buka, Harga Saat Ini]
