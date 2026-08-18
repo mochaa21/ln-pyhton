505713867353f6dad3efb308369db55381ce087a
mochaa21 <mochamadsyahid573@gmail.com>
2026-08-18 21:36:44 +0700

    update: main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ player_action = ['W', 'X', 'A', 'B', 'C', 'D']
 action_join = " ".join(player_action)
 
 print(action_join)
+
+r = 1
+c = 0
+
+isTuple = (r, c)
+
+unpackingTuple = 0
+
+r, c = unpackingTuple
+
+print(unpackingTuple)
